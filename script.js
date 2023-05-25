af1209139ae5dd6ea7850b6c39a28ec05ba4cdf4
Marlon Victor <119461277+Marlon-09@users.noreply.github.com>
2023-05-25 18:44:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function abrirMenu(){
+    let BarraMenuAberto = document.getElementById("barra-menu");
+
+    if(BarraMenuAberto.style.width == "0px"){
+        BarraMenuAberto.style.width = "300px"; 
+    }
+    else{
+        BarraMenuAberto.style.width = "0px";
+    }
+}
